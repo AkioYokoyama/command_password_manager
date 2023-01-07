bf65af5db615c0d682441691055036b754211fd2
akio.yokoyama.yoika@gmail.com <Yoika>
2023-01-07 16:05:53 +0900

    Add unique key to key column

--- migrations/20230105220000_create_table_passwords.sql
+++ migrations/20230105220000_create_table_passwords.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS passwords
 (
     id          INTEGER PRIMARY KEY NOT NULL,
-    key         TEXT                NOT NULL,
+    key         TEXT    UNIQUE      NOT NULL,
     password    TEXT                NOT NULL
 );
